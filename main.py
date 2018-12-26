4c549fb716f2a6d4661f275c9fdb219ef25508a8
mohanson <mohanson@outlook.com>
2018-12-26 17:22:25 +0800

    2018-12-26 17:22:25

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import wasmi
 
 path = './tests/data/add.wasm'
-path = r"C:\Users\mohan\Downloads\program (4).wasm"
 
 with open(path, 'rb') as f:
     mod = wasmi.Mod.from_reader(f)
 vm = wasmi.Vm(mod)
-r = vm.exec('foo', [10])
+r = vm.exec('add', [40, 2])
 print(r)
