0c48bc576b4ad3328234b7a3428b7b13aa79c3c1
mohanson <mohanson@outlook.com>
2018-12-26 11:21:51 +0800

    2018-12-26 11:21:51

---
+++ wasmi/error.py
@@ -0,0 +1,5 @@
+class InvalidMagicNumber(Exception):
+    pass
+
+class Unreachable(Exception):
+    pass
--- wasmi/stack.py
+++ wasmi/stack.py
@@ -16,25 +16,35 @@ class Entry:
 
     @classmethod
     def from_i32(cls, n):
-        data = struct.pack('<i', n)
-        data = data + bytearray([0x00, 0x00, 0x00, 0x00])
+        data = struct.pack('>i', n)
+        data = bytearray([0x00, 0x00, 0x00, 0x00]) + data
         return Entry(data, wasmi.opcodes.VALUE_TYPE_I32)
 
     @classmethod
     def from_i64(cls, n):
-        data = struct.pack('<q', n)
+        data = struct.pack('>q', n)
+        return Entry(data, wasmi.opcodes.VALUE_TYPE_I64)
+
+    @classmethod
+    def from_u32(cls, n):
+        data = struct.pack('>I', n)
+        data = bytearray([0x00, 0x00, 0x00, 0x00]) + data
+        return Entry(data, wasmi.opcodes.VALUE_TYPE_I32)
+
+    @classmethod
+    def from_u64(cls, n):
+        data = struct.pack('>Q', n)
         return Entry(data, wasmi.opcodes.VALUE_TYPE_I64)
 
     @classmethod
     def from_f32(cls, n):
-        data = struct.pack('<f', n)
-        data = data + bytearray([0x00, 0x00, 0x00, 0x00])
+        data = struct.pack('>f', n)
+        data = bytearray([0x00, 0x00, 0x00, 0x00]) + data
         return Entry(data, wasmi.opcodes.VALUE_TYPE_F32)
 
     @classmethod
     def from_f64(cls, n):
-        data = struct.pack('<d', n)
-        data = data + bytearray([0x00, 0x00, 0x00, 0x00])
+        data = struct.pack('>d', n)
         return Entry(data, wasmi.opcodes.VALUE_TYPE_F64)
 
     @classmethod
@@ -50,16 +60,22 @@ class Entry:
         raise NotImplementedError()
 
     def into_i32(self):
-        return struct.unpack('<i', self.data[:4])[0]
+        return struct.unpack('>i', self.data[4:])[0]
 
     def into_i64(self):
-        return struct.unpack('<q', self.data)[0]
+        return struct.unpack('>q', self.data)[0]
+
+    def into_u32(self):
+        return struct.unpack('>I', self.data[4:])[0]
+
+    def into_u64(self):
+        return struct.unpack('>Q', self.data)[0]
 
     def into_f32(self):
-        return struct.unpack('<f', self.data[:4])[0]
+        return struct.unpack('>f', self.data[4:])[0]
 
     def into_f64(self):
-        return struct.unpack('<d', self.data)[0]
+        return struct.unpack('>d', self.data)[0]
 
     def into_val(self):
         if self.kind == wasmi.opcodes.VALUE_TYPE_I32:
@@ -86,23 +102,35 @@ class Stack:
     def add_i32(self, n):
         self.add(Entry.from_i32(wasmi.common.into_i32(n)))
 
-    def pop_i32(self):
-        return self.pop().into_i32()
-
     def add_i64(self, n):
         self.add(Entry.from_i64(wasmi.common.into_i64(n)))
 
-    def pop_i64(self):
-        return self.pop().into_i64()
+    def add_u32(self, n):
+        self.add(Entry.from_u32(wasmi.common.into_u32(n)))
+
+    def add_u64(self, n):
+        self.add(Entry.from_u64(wasmi.common.into_u64(n)))
 
     def add_f32(self, n):
         self.add(Entry.from_f32(wasmi.common.into_f32(n)))
 
-    def pop_f32(self):
-        return self.pop().into_f32()
-
     def add_f64(self, n):
         self.add(Entry.from_f64(wasmi.common.into_f64(n)))
+
+    def pop_i32(self):
+        return self.pop().into_i32()
+
+    def pop_i64(self):
+        return self.pop().into_i64()
+
+    def pop_u32(self):
+        return self.pop().into_u32()
+
+    def pop_u64(self):
+        return self.pop().into_u64()
+
+    def pop_f32(self):
+        return self.pop().into_f32()
 
     def pop_f64(self):
         return self.pop().into_f64()
