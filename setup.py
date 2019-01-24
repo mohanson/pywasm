cbcf0604cef0e770d054797d07373199dc6baee3
mohanson <mohanson@outlook.com>
2019-01-24 09:53:23 +0800

    Long description on pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+import os.path
 import setuptools
+
+root = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(root, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name='pywasm',
-    version='0.4.1',
+    version='0.4.4',
     url='https://github.com/mohanson/pywasm',
     license='WTFPL',
     author='mohanson',
     author_email='mohanson@outlook.com',
     description='WebAssembly Interpreter by pure Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['pywasm'],
 )
