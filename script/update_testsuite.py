5a0fb45c529aefeb91ea3aa6119c44b167df3bfa
mohanson <mohanson@outlook.com>
2020-04-23 11:52:47 +0800

    Submit for stash 02

--- script/update_testsuite.py
+++ script/update_testsuite.py
@@ -3,9 +3,11 @@ import os
 import shutil
 import subprocess
 import sys
+import tempfile
 
 wabt = 'https://github.com/WebAssembly/wabt'
 testsuite = 'https://github.com/WebAssembly/testsuite'
+testsuite_commit_id = 'da56298dddb441d1af38492ee98fe001e625d156'
 wast2json = shutil.which('wast2json')
 
 if not wast2json:
@@ -19,16 +21,13 @@ def call(cmd: str):
     return subprocess.call(cmd, shell=True)
 
 
-shutil.rmtree('./res/testsuite', ignore_errors=True)
-shutil.rmtree('./res/testsuite_wasm', ignore_errors=True)
-os.makedirs('./res', exist_ok=True)
-os.mkdir('./res/testsuite_wasm')
-
-os.chdir('./res')
-call(f'git clone {testsuite}')
-os.chdir('..')
+call('rm -rf ./res/testsuite')
+call('mkdir -p ./res')
+call(f'cd ./res && git clone {testsuite}')
+call(f'cd ./res/testsuite && git checkout -b {testsuite_commit_id} {testsuite_commit_id}')
+call(f'cd ./res/testsuite && mkdir spec')
 
 for e in glob.glob('./res/testsuite/*.wast'):
     a, _ = os.path.splitext(os.path.basename(e))
-    os.makedirs(f'./res/testsuite_wasm/{a}')
-    call(f'wast2json --enable-all {e} -o ./res/testsuite_wasm/{a}/{a}.json')
+    os.makedirs(f'./res/testsuite/spec/{a}')
+    call(f'wast2json --enable-all {e} -o ./res/testsuite/spec/{a}/{a}.json')
--- test/test_spec.py
+++ test/test_spec.py
@@ -96,4 +96,27 @@ def test_spec():
             raise NotImplementedError
 
 
-test_spec()
+# test_spec()
+
+def case(path: str):
+    case_name = os.path.basename(path)
+    json_path = os.path.join(path, case_name + '.json')
+    with open(json_path, 'r') as f:
+        case_data = json.load(f)
+    for e in case_data['commands']:
+        print(e)
+        if e['type'] == 'module':
+            print(e)
+            continue
+        if e['type'] == 'assert_return':
+            print(e)
+            continue
+        if e['type'] == 'assert_trap':
+            print(e)
+            continue
+        if e['type'] == 'assert_malformed':
+            print(e)
+            continue
+
+
+case('./test/spec/address')
