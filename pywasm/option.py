7a705b7a8f0870e9e6ec8de32fa7eb269df66608
mohanson <mohanson@outlook.com>
2020-12-23 23:34:18 +0800

    Fix a typing error

--- pywasm/option.py
+++ pywasm/option.py
@@ -1,11 +1,13 @@
 import typing
 
+from . import binary
+
 
 class Option:
     # A option consists of the current execution state
     def __init__(self):
-        # A function used to count cycle consumption, the parameter is the opcode of the instruction
-        self.instruction_cycle_func: typing.Callable[[int], int] = lambda _: 1
+        # A function used to count cycle consumption
+        self.instruction_cycle_func: typing.Callable[[binary.Instruction], int] = lambda _: 1
         # If cycle exceeded limit, an out of gas error will be throwed
         self.cycle_limit = 0
         # Count the currently used cycles
