dd53ea71a6ac2d28dd221d8561e02da1f2d0a0d1
mohanson <mohanson@outlook.com>
2025-06-26 11:18:35 +0800

    Update spectest

--- script/spectest.py
+++ script/spectest.py
@@ -12,8 +12,8 @@ call('cd res && rm -rf spectest')
 if not os.path.exists('res/spec'):
     call('cd res && git clone https://github.com/WebAssembly/spec')
 # When update spectest, we can increase the commit id in small steps.
-# Use: git log --reverse abc8d16dcb4b36f818af9ee2fa9b1bf0b50503cb.. -- test/core
-call('cd res/spec && git checkout abc8d16dcb4b36f818af9ee2fa9b1bf0b50503cb')
+# Use: git log --reverse 1041527d508218acc40f0278d4abc3be9ba5e3bd.. -- test/core
+call('cd res/spec && git checkout 1041527d508218acc40f0278d4abc3be9ba5e3bd')
 call('cd res && cp -R spec/test/core spectest')
 
 os.chdir('res/spectest')
