598b1c70d265aac91bdb5e4a5233c6bff5602783
Mohanson <mohanson@outlook.com>
2025-07-08 16:03:09 +0800

    Support wasi preview 1 on macOS

--- script/build_wabt.py
+++ script/build_wabt.py
@@ -1,7 +1,10 @@
 import contextlib
 import os
+import platform
 import subprocess
+import tarfile
 import typing
+import urllib.request
 
 
 def call(cmd: str) -> subprocess.CompletedProcess[bytes]:
@@ -18,8 +21,20 @@ def cd(dst: str) -> typing.Generator[None, typing.Any, None]:
 
 
 version = '1.0.37'
+url = ''
+match platform.system().lower():
+    case 'darwin':
+        url = f'https://github.com/WebAssembly/wabt/releases/download/{version}/wabt-{version}-macos-14.tar.gz'
+    case 'linux':
+        url = f'https://github.com/WebAssembly/wabt/releases/download/{version}/wabt-{version}-ubuntu-20.04.tar.gz'
+    case 'windows':
+        url = f'https://github.com/WebAssembly/wabt/releases/download/{version}/wabt-{version}-windows.tar.gz'
+    case _:
+        assert 0
+out = os.path.basename(url)
 with cd('res'):
-    call(f'wget https://github.com/WebAssembly/wabt/releases/download/{version}/wabt-{version}-ubuntu-20.04.tar.gz')
-    call(f'tar -zxvf wabt-{version}-ubuntu-20.04.tar.gz')
-    call(f'rm -rf wabt-{version}-ubuntu-20.04.tar.gz')
-    call(f'mv wabt-{version} wabt')
+    urllib.request.urlretrieve(url, out)
+    with tarfile.open(out, 'r:gz') as tar:
+        tar.extractall()
+    os.remove(out)
+    os.rename(f'wabt-{version}', 'wabt')
