a6729b62ffb80af6a0b6e6e7a683ba6f4215eb6d
mohanson <mohanson@outlook.com>
2025-07-13 16:34:29 +0800

    2025-07-13 16:34:29

--- pywasm/wasi.py
+++ pywasm/wasi.py
@@ -1177,8 +1177,12 @@ class Preview1:
             return [self.ERRNO_ISDIR]
         except NotADirectoryError:
             return [self.ERRNO_NOTDIR]
-        except OSError:
-            return [self.ERRNO_LOOP]
+        except OSError as e:
+            if platform.system().lower() == 'darwin' and e.errno == 62:
+                return [self.ERRNO_NOTEMPTY]
+            if platform.system().lower() == 'linux' and e.errno == 40:
+                return [self.ERRNO_LOOP]
+            raise e
         rights_base = args[5]
         rights_root = args[6]
         # Drectory does not have the seek right.
