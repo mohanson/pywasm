8cc535bb3840bcdc8bb6e0e9805cb9d81de3774f
Mohanson <mohanson@outlook.com>
2025-09-12 14:15:21 +0800

    Refactor log

--- pywasm/log.py
+++ pywasm/log.py
@@ -4,10 +4,10 @@ lvl = 0
 
 
 def debugln(*args):
-    if lvl:
+    if lvl > 0:
         println(*args)
 
 
 def println(*args):
     pre = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-    print(pre, *args)
+    print(pre, 'pywasm:', *args)
